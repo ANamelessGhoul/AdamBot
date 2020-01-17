015d2983c3a33defc851f8816858b90643775f9c
Tecelli Akıntuğ <tecelli.e.a@gmail.com>
2020-01-17 18:36:47 +0300

    Added general gezdir func
    Added voice dependencies

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ dotenv.config();
 
 client.on('ready', () => {
     if(!(process.env.PREFIX != undefined))
-        console.error('No prefix set')
+        console.error('No prefix set');
 
-    console.log('Bot online!')
+    console.log('Bot online!');
 })
 
 client.on('message', message => {
@@ -17,19 +17,27 @@ client.on('message', message => {
 
     switch (command) {
         case 'help':
-            help(message.member)
+            help(message.member);
             break;
+        case 'ping':
+            ping(message);
+            break;
+        /*
         case 'gel':
-            gel(message)
+            gel(message);
             break;
+        */
         case 'çıkar beni':
-            cikar(message)
+            cikar(message);
             break;
         case 'gezdir beni':
-            gezdir(message)
+            gezdir(message);
+            break;
+        case 'gezdir bizi':
+            gezdirAll(message);
             break;
         default:
-            console.log('Unrecognized command: ' + command)
+            console.log('Unrecognized command: ' + command);
             break;
     }
 })
@@ -38,43 +46,88 @@ function help(member){
     const embed = new Discord.RichEmbed()
       .setTitle('AdamBot Guide:')
       .setColor(0xFF0000)
-      .setDescription('Bot call prefix: "'+ process.env.PREFIX +'"\n"gel": Basic ping function\n"çıkar beni": Removes you from voice channel\n"gezdir beni": Scrolls you through all voice channels');
+      .setDescription('Bot call prefix: "'+ process.env.PREFIX +`"\n
+      "ping": Basic ping function\n
+      "gel": Basic ping function\n
+      "çıkar beni": Removes you from voice channel\n
+      "gezdir beni": Scrolls you through all voice channels'
+      `);
 
     member.send(embed);
 
 }
 
+function ping(message){
+    message.reply('Siktir!');
+}
+
 function gel(message){
-    message.reply('Siktir!')
+    var member = message.member;
+    if(member.voiceChannelID == null)
+    {
+        message.reply('Sikdir be! Sen nerdesin da geleyim!');
+        return;
+    }
+
+    member.voiceChannel.join();
+
 }
 
 function cikar(message){
-    if(message.member.voiceChannelID == null)
-        message.reply('Ma nerdesin da çıkaracam be!')
-    else
+    if(!message.member.voiceChannel)
     {
-        message.reply('Ey be tamam!')
-        message.member.setVoiceChannel(null)
+        message.reply('Ma nerdesin da çıkaracam be!');
+        return;
     }
+    
+    message.reply('Ey be tamam!');
+    message.member.setVoiceChannel(null);
+    
 }
 
 function gezdir(message)
 {
-    var user = message.member
+    var user = message.member;
 
-    if(user.voiceChannelID == null)
+    if(!user.voiceChannel)
     {
-        message.reply('Girmeden bir yere nasıl gezdireyim be seni!')
+        message.reply('Girmeden bir yere nasıl gezdireyim be seni!');
         return;
     } 
 
-    var firstChannel = user.voiceChannel
+    var firstChannel = user.voiceChannel;
 
     message.guild.channels.filter(g => g.type == 'voice').forEach(element => {
-        user.setVoiceChannel(element)
+        user.setVoiceChannel(element);
     })
 
-    user.setVoiceChannel(firstChannel)    
+    user.setVoiceChannel(firstChannel)    ;
+}
+
+function gezdirAll(message)
+{
+    var user = message.member;
+
+    if(!user.voiceChannel)
+    {
+        message.reply('Girmeden bir yere nasıl gezdireyim be sizi!');
+        return;
+    } 
+
+    var firstChannel = user.voiceChannel;
+
+    var allUsers = user.voiceChannel.members;
+
+    message.guild.channels.filter(g => g.type == 'voice').forEach(element => {
+        allUsers.forEach(singleUser => {
+            singleUser.setVoiceChannel(element);
+        })
+        
+    })
+
+    allUsers.forEach(singleUser => {
+        singleUser.setVoiceChannel(firstChannel);
+    })  
 }
     
 

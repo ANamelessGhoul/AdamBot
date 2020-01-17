780393759c5724b517b87c47d5ab17297e5b88c0
Tecelli Akıntuğ <tecelli.e.a@gmail.com>
2020-01-17 11:59:01 +0300

    Cleaned up code to work on single event and ignore invalid messages

--- index.js
+++ index.js
@@ -1,59 +1,68 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-const token = 'NjY3MzMyNjk4NDY0ODQ1ODI1.XiBNag.05SyXVJFKOsh5GVK99EyvrTkRMs'
+const token = 'NjY3MzMyNjk4NDY0ODQ1ODI1.XiBNag.05SyXVJFKOsh5GVK99EyvrTkRMs' //release
+//const token = 'NjY3NjQwMDM5NjI2MjQ0MTIx.XiFqXA.yQPRpa5Z_ZlxEXzSzaoM-8FT_Yo' //beta
 
-const botCall = 'Adam! '
+
+const botPrefix = 'Adam!'
 
 client.on('ready', () => {
     console.log('Bot online!')
 })
 
 client.on('message', message => {
-    if(message.content === botCall + 'gel'){
-        message.reply('Siktir!')
+    if (!message.content.startsWith(botPrefix) || !message.guild) return;
+    //const command = message.content.split(' ')[1];
+    const command = message.content.split(' ').slice(1).join(' ');
+
+    switch (command) {
+        case 'gel':
+            gel(message)
+            break;
+        case 'çıkar beni':
+            cikar(message)
+            break;
+        case 'gezdir beni':
+            gezdir(message)
+            break;
+        default:
+            break;
     }
 })
 
-client.on('message', message => {
-    if (message.content === botCall + 'çıkar beni') 
+function gel(message){
+    message.reply('Siktir!')
+}
+
+function cikar(message){
+    if(message.member.voiceChannelID == null)
+        message.reply('Ma nerdesin da çıkaracam be!')
+    else
     {
-        if(message.member.voiceChannelID == null)
-            message.reply('Ma nerdesin da çıkaracam be!')
-        else
-        {
-            message.reply('Ey be tamam!')
-            message.member.setVoiceChannel(null)
-        }
-            
+        message.reply('Ey be tamam!')
+        message.member.setVoiceChannel(null)
     }
-})
+}
 
-client.on('message', message => {
-    if (message.content === botCall + 'gezdir beni') 
+function gezdir(message)
+{
+    var user = message.member
+
+    if(user.voiceChannelID == null)
     {
-        /*
-        client.channels.forEach(element => {
-            message.member.setVoiceChannel(element)
-        }); 
-        */
-        
-        let firstChannel
+        message.reply('Girmeden bir yere nasıl gezdireyim be seni!')
+        return;
+    } 
 
-        
-        if(message.member.voiceChannelID != undefined)
-            firstChannel = message.member.voiceChannel
+    var firstChannel = user.voiceChannel
 
-        message.guild.channels.filter(g => g.type == 'voice').forEach(element => {
+    message.guild.channels.filter(g => g.type == 'voice').forEach(element => {
+        user.setVoiceChannel(element)
+    })
 
-            message.member.setVoiceChannel(element)
-        })
-
-        if(firstChannel != undefined)
-            message.member.setVoiceChannel(firstChannel)
-
-        
-    }
-})
+    user.setVoiceChannel(firstChannel)    
+}
+    
 
 client.login(token);
